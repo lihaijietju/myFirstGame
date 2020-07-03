5592a78f6ea9c91c80c82e877a15b17a586761b6
lihaijie <943706048@qq.com>
2020-07-03 21:43:51 +0800

    save

--- node/api/cityApi.js
+++ node/api/cityApi.js
@@ -13,7 +13,7 @@ router.get('/getAccount', async (ctx, next) => {
     if (ctx.headers.token !== utility.md5(ctx.query.account)) {
         return;
     }
-    if((+new Date() - ctx.query.nowTime) > 10000){
+    if((+new Date() - ctx.query.nowTime || +new Date()) > 10000){
         ctx.response.body = {
             code: 0,
             message: '成功',
