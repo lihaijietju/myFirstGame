db40f3b5f96f297692430e4278e015d09378278d
lihaijie <943706048@qq.com>
2020-07-13 22:55:03 +0800

    save

--- node/api/battleApi.js
+++ node/api/battleApi.js
@@ -279,9 +279,9 @@ router.post('/onceCreateBattle', async (ctx, next) => {
             id: battleList[i].id
         };
         if(+targetUser.monthcarddays > 0){
-            battleList[i].totaltime = 6 * 60 * 60;
+            obj.totaltime = 6 * 60 * 60;
         }else{
-            battleList[i].totaltime = 3 * 60 * 60;
+            obj.totaltime = 3 * 60 * 60;
         }
         targetList.push(obj);
     }
